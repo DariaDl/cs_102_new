514281d4cef46cbed035127cca069f03410284ef
DariaDl <59313644+DariaDl@users.noreply.github.com>
2020-12-17 18:21:34 +0000

    Update index.py

--- homework04/pyvcs/index.py
+++ homework04/pyvcs/index.py
@@ -131,9 +131,7 @@ def ls_files(gitdir: pathlib.Path, details: bool = False) -> None:
             print(entry.path)
 
 
-def update_index(
-    gitdir: pathlib.Path, paths: tp.List[pathlib.Path], write: bool = True
-) -> None:
+def update_index(gitdir: pathlib.Path, paths: tp.List[pathlib.Path], write: bool = True) -> None:
     entry = {entry.name: entry for entry in read_index(gitdir)}
     for path in paths:
         if str(path) in entry:
