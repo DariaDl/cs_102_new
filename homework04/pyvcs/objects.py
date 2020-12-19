3bad1c23b1f696d221bc0e8ba3bf1f5f6fc9d736
DariaDl <59313644+DariaDl@users.noreply.github.com>
2020-12-19 14:33:18 +0300

    Update objects.py

--- homework04/pyvcs/objects.py
+++ homework04/pyvcs/objects.py
@@ -12,19 +12,23 @@ from pyvcs.repo import repo_find
 
 def hash_object(data: bytes, fmt: str, write: bool = False) -> str:
     objects = "objects"
-    final = hashlib.sha1((fmt + " " + str(len(data))).encode() + b"\00" + data).hexdigest()
+    sha = hashlib.sha1(
+        (fmt + " " + str(len(data))).encode() + b"\00" + data
+    ).hexdigest()
     if write:
         gitdir = repo_find()
-        if not (gitdir / objects / final[:2]).exists():
-            (gitdir / objects / final[:2]).mkdir()
-        with (gitdir / objects / final[:2] / final[2:]).open("wb") as file:
-            file.write(zlib.compress((fmt + " " + str(len(data))).encode() + b"\00" + data))
-    return final
+        if not (gitdir / objects / sha[:2]).exists():
+            (gitdir / objects / sha[:2]).mkdir()
+        with (gitdir / objects / sha[:2] / sha[2:]).open("wb") as file:
+            file.write(
+                zlib.compress((fmt + " " + str(len(data))).encode() + b"\00" + data)
+            )
+    return sha
 
 
 def resolve_object(obj_name: str, gitdir: pathlib.Path) -> tp.List[str]:
     objects = "objects"
-    if not 4 < len(obj_name) < 40:
+    if 40 < len(obj_name) < 4:
         raise Exception(f"Not a valid object name {obj_name}")
     gitdir = repo_find()
     obj_list = []
@@ -32,9 +36,9 @@ def resolve_object(obj_name: str, gitdir: pathlib.Path) -> tp.List[str]:
         if not dir.is_dir():
             continue
         for file in dir.glob("*"):
-            cur_obj_name = file.parent.name + file.name
-            if obj_name == cur_obj_name[: len(obj_name)]:
-                obj_list.append(cur_obj_name)
+            real_obj_name = file.parent.name + file.name
+            if obj_name == real_obj_name[: len(obj_name)]:
+                obj_list.append(real_obj_name)
     if not obj_list:
         raise Exception(f"Not a valid object name {obj_name}")
     return obj_list
@@ -90,10 +94,10 @@ def find_tree_files(tree_sha: str, gitdir: pathlib.Path) -> tp.List[tp.Tuple[str
 def commit_parse(raw: bytes, start: int = 0, dct=None):
     ret_val: tp.Dict[str, tp.Any]
     ret_val = {"message": []}
-    for element in raw.decode().split("\n"):
-        if element.startswith(("tree", "parent", "author", "committer")):
-            name, val = element.split(" ", maxsplit=1)
+    for i in raw.decode().split("\n"):
+        if i.startswith(("tree", "parent", "author", "committer")):
+            name, val = i.split(" ", maxsplit=1)
             ret_val[name] = val
         else:
-            ret_val["message"].append(element)
+            ret_val["message"].append(i)
     return ret_val
